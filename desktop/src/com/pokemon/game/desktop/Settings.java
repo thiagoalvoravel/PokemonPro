d0c38b8b6774803d367d18ddb41e0ab9e9546322
Thiago Alvoravel <t.alvoravel@gmail.com>
2017-05-18 14:27:08 -0300

    Alteração no tamanho da tela

--- desktop/src/com/pokemon/game/desktop/Settings.java
+++ desktop/src/com/pokemon/game/desktop/Settings.java
@@ -2,7 +2,7 @@ package com.pokemon.game.desktop;
 
 public class Settings {
 
-   public static int TILE_SIZE = 7;
+   public static int TILE_SIZE = 8;
    public static float SCALE = 2f;
    public static float SCALED_TILE_SIZE = TILE_SIZE * SCALE;
     
