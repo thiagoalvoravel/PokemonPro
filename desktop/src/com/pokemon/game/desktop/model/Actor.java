961ab5e6533463e5b28a337e7ed45d3ca6299111
Thiago Alvoravel <t.alvoravel@gmail.com>
2017-05-18 11:20:15 -0300

    Acréscimo de métodos getters e setters

--- desktop/src/com/pokemon/game/desktop/model/Actor.java
+++ desktop/src/com/pokemon/game/desktop/model/Actor.java
@@ -5,24 +5,24 @@ import com.badlogic.gdx.math.Interpolation;
 import com.pokemon.game.desktop.util.AnimationSet;
 
 public class Actor {
-   
-   private TileMap map; 
-   private int x;
-   private int y;
-   private DIRECTION facing;
-   
-   private float worldX, worldY;
-   private int srcX, srcY;
-   private int destX, destY;
-   private float animTimer;
-   private float ANIM_TIME = 0.3f;
-   
-   private float walkTimer;
-   private boolean moveRequestThisFrame;
-   
-   private ACTOR_STATE state;
-   
-   private AnimationSet animations;
+
+    private TileMap map;
+    private int x;
+    private int y;
+    private DIRECTION facing;
+
+    private float worldX, worldY;
+    private int srcX, srcY;
+    private int destX, destY;
+    private float animTimer;
+    private float ANIM_TIME = 0.3f;
+
+    private float walkTimer;
+    private boolean moveRequestThisFrame;
+
+    private ACTOR_STATE state;
+
+    private AnimationSet animations;
 
     public Actor(TileMap map, int x, int y, AnimationSet animations) {
         this.map = map;
@@ -35,24 +35,23 @@ public class Actor {
         this.state = ACTOR_STATE.STANDING;
         this.facing = DIRECTION.SOUTH;
     }
-    
-    public enum ACTOR_STATE  {
+
+    public enum ACTOR_STATE {
         WALKING,
-        STANDING,
-        ;    
+        STANDING,;
     }
-    
+
     public void update(float delta) {
-        if(state == ACTOR_STATE.WALKING) {
+        if (state == ACTOR_STATE.WALKING) {
             animTimer += delta;
             walkTimer += delta;
-            worldX = Interpolation.linear.apply(srcX, destX, animTimer/ANIM_TIME);
-            worldY = Interpolation.linear.apply(srcY, destY, animTimer/ANIM_TIME);
-            if(animTimer > ANIM_TIME) {
-                float leftOverTime = animTimer-ANIM_TIME;  
+            worldX = Interpolation.linear.apply(srcX, destX, animTimer / ANIM_TIME);
+            worldY = Interpolation.linear.apply(srcY, destY, animTimer / ANIM_TIME);
+            if (animTimer > ANIM_TIME) {
+                float leftOverTime = animTimer - ANIM_TIME;
                 walkTimer -= leftOverTime;
                 finishMove();
-                if(moveRequestThisFrame){
+                if (moveRequestThisFrame) {
                     move(facing);
                 } else {
                     walkTimer = 0f;
@@ -69,50 +68,50 @@ public class Actor {
     public float getWorldY() {
         return worldY;
     }
-    
+
     public TextureRegion getSprite() {
-        if(state == ACTOR_STATE.WALKING) {
+        if (state == ACTOR_STATE.WALKING) {
             return (TextureRegion) animations.getWalking(facing).getKeyFrame(walkTimer);
-        }else if(state == ACTOR_STATE.STANDING){
+        } else if (state == ACTOR_STATE.STANDING) {
             return animations.getStanding(facing);
         }
         return animations.getStanding(DIRECTION.SOUTH);
     }
-    
+
     public boolean move(DIRECTION dir) {
-       if (state == ACTOR_STATE.WALKING) {
-           if(facing == dir) {
-              moveRequestThisFrame = true;    
-           }
-          return false;   
-       }
-       if(x+dir.getDX() >= map.getWidth() || x+dir.getDX() < 0 || y+dir.getDY() >= map.getHeight() || y+dir.getDY() < 0) {
-          return false;
-       }
-       if(map.getTerrenos(x+dir.getDX(), y+dir.getDY()).getActor() != null){
-          return false;
-       }
-       initializeMove(dir);
-       
-       map.getTerrenos(x, y).setActor(null);
-       x += dir.getDX();
-       y += dir.getDY();
-       map.getTerrenos(x, y).setActor(this);
-       return true;
+        if (state == ACTOR_STATE.WALKING) {
+            if (facing == dir) {
+                moveRequestThisFrame = true;
+            }
+            return false;
+        }
+        if (x + dir.getDX() >= map.getWidth() || x + dir.getDX() < 0 || y + dir.getDY() >= map.getHeight() || y + dir.getDY() < 0) {
+            return false;
+        }
+        if (map.getTerrenos(x + dir.getDX(), y + dir.getDY()).getActor() != null) {
+            return false;
+        }
+        initializeMove(dir);
+
+        map.getTerrenos(x, y).setActor(null);
+        x += dir.getDX();
+        y += dir.getDY();
+        map.getTerrenos(x, y).setActor(this);
+        return true;
     }
-    
+
     private void initializeMove(DIRECTION dir) {
-           this.facing = dir;
-           this.srcX =  x;
-           this.srcY =  y;
-           this.destX = x+dir.getDX();
-           this.destY = y+dir.getDY();
-           this.worldX = x;
-           this.worldY = y;
-           animTimer = 0f;
-           state = ACTOR_STATE.WALKING;
+        this.facing = dir;
+        this.srcX = x;
+        this.srcY = y;
+        this.destX = x + dir.getDX();
+        this.destY = y + dir.getDY();
+        this.worldX = x;
+        this.worldY = y;
+        animTimer = 0f;
+        state = ACTOR_STATE.WALKING;
     }
-    
+
     private void finishMove() {
         state = ACTOR_STATE.STANDING;
         this.worldX = destX;
@@ -121,7 +120,7 @@ public class Actor {
         this.srcY = 0;
         this.destX = 0;
         this.destY = 0;
-                
+
     }
 
     public int getX() {
@@ -131,9 +130,65 @@ public class Actor {
     public int getY() {
         return y;
     }
+
+    public void setX(int x_) {
+        this.x = x_;
+    }
+
+    public void setY(int y_) {
+        this.y = y_;
+    }
+
+    public DIRECTION getFacing() {
+        return this.facing;
+    }
+
+    public void setFacing(DIRECTION dir) {
+        this.facing = dir;
+    }
+
+    public void setState(ACTOR_STATE state_) {
+        this.state = state_;
+    }
     
+    public int getSrcX() {
+        return srcX;
+    }
+
+    public void setSrcX(int srcX) {
+        this.srcX = srcX;
+    }
+
+    public int getSrcY() {
+        return srcY;
+    }
+
+    public void setSrcY(int srcY) {
+        this.srcY = srcY;
+    }
+
+    public int getDestX() {
+        return destX;
+    }
+
+    public void setDestX(int destX) {
+        this.destX = destX;
+    }
+
+    public int getDestY() {
+        return destY;
+    }
+
+    public void setDestY(int destY) {
+        this.destY = destY;
+    }
     
-   
-  
-    
+    public boolean isMoveRequestThisFrame() {
+        return moveRequestThisFrame;
+    }
+
+    public void setMoveRequestThisFrame(boolean moveRequestThisFrame) {
+        this.moveRequestThisFrame = moveRequestThisFrame;
+    }
+
 }
