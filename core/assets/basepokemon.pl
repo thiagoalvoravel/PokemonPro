d1afe8e140618944f253c79252fe62666852aadf
FillipeCampos <fillipecampos19@gmail.com>
2017-06-02 00:20:32 -0300

    Inserção de Fato e Regra Teste

--- core/assets/basepokemon.pl
+++ core/assets/basepokemon.pl
@@ -3,8 +3,10 @@
 %FATOS
 coordenada('sul',0,-1).
 coordenada('norte',0,1).
+correr('sim').
 
 %REGRAS
 andar(Direcao, Coordx,Coordy) :- coordenada(Direcao,Coordx,Coordy).
+addcorrer(Direcao) :- correr(Direcao).
 
 
